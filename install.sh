87d708dfe7a564227480391fc1cf3e41123a256a
dmalikov <color404051h@gmail.com>
2011-09-15 13:17:12 +0400

    install script added

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ln -sf --verbose "$(pwd)/.vimrc"     ~/.vimrc
+ln -sf --verbose "$(pwd)/.apvlvrc"   ~/.apvlvrc
+ln -sf --verbose "$(pwd)/.conkyrc"   ~/.conkyrc
+ln -sf --verbose "$(pwd)/.screenrc"  ~/.screenrc
+
+echo "done!"

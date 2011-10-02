d350b5786d68844a9e989fed813ca894e4e0eeaa
Dmitry Malikov <color404051h@gmail.com>
2011-10-03 01:45:15 +0400

    gvimrc is added

---
+++ .gvimrc
@@ -0,0 +1,8 @@
+set cursorline " current line highlighting
+set guifont=Terminus\ 14
+set guioptions-=m " remove menu
+set guioptions-=T " remove toolbar
+set guioptions-=r " remove scrollbar
+
+source ~/.vim/gvim_developer_rc.vi
+
--- install.sh
+++ install.sh
@@ -4,5 +4,7 @@ ln -sf --verbose "$(pwd)/.vimrc"     ~/.vimrc
 ln -sf --verbose "$(pwd)/.apvlvrc"   ~/.apvlvrc
 ln -sf --verbose "$(pwd)/.conkyrc"   ~/.conkyrc
 ln -sf --verbose "$(pwd)/.screenrc"  ~/.screenrc
+ln -sf --verbose "$(pwd)/.gvimrc"    ~/.gvimrc
 
 echo "done!"
+

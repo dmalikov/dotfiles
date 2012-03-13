5b0a334230c37fdd61aafcb42951a289e8c02c80
Dmitry Malikov <color404051h@gmail.com>
2012-03-14 01:45:34 +0400

    Remove dots from filenames.

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-ln -sf --verbose "$(pwd)/.vimrc"     ~/.vimrc
-ln -sf --verbose "$(pwd)/.apvlvrc"   ~/.apvlvrc
-ln -sf --verbose "$(pwd)/.conkyrc"   ~/.conkyrc
-ln -sf --verbose "$(pwd)/.screenrc"  ~/.screenrc
-ln -sf --verbose "$(pwd)/.gvimrc"    ~/.gvimrc
+ln -sf --verbose "$(pwd)/vimrc" ~/.vimrc
+ln -sf --verbose "$(pwd)/apvlvrc" ~/.apvlvrc
+ln -sf --verbose "$(pwd)/conkyrc" ~/.conkyrc
+ln -sf --verbose "$(pwd)/screenrc" ~/.screenrc
+ln -sf --verbose "$(pwd)/gvimrc" ~/.gvimrc
+ln -sf --verbose "$(pwd)/xinitrc" ~/.xinitrc
+ln -sf --verbose "$(pwd)/Xdefaults" ~/.Xdefaults
+ln -sf --verbose "$(pwd)/pentadactylrc" ~/.pentadactylrc
+ln -sf --verbose "$(pwd)/gitconfig" ~/.gitconfig
 
 echo "done!"
 

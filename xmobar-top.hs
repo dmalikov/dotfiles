1a243f4baf8bcbf308832e2d0e504c2eedba67c2
Dmitry Malikov <color404051h@gmail.com>
2013-01-06 00:23:36 +0400

    XMobar: finally add xmobar-top

---
+++ xmobar-top.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE UnicodeSyntax #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+module Main (main) where
+
+import Application
+import Text.Printf
+
+type Color = String
+
+main :: IO ()
+main = xmobar Config
+  { hideOnStart = False
+  , persistent = True
+  , lowerOnStart = False
+  , font = "-*-terminus-medium-r-normal--12-*"
+  , border = NoBorder
+  , borderColor = backgroundColor
+  , bgColor = backgroundColor
+  , fgColor = foregroundColor
+  , position = TopSize C 100 20
+  , commands = [ Run $ TopMem
+     [ "-t", concatMap nameAndMem [1..10]
+     ] 30
+     , Run $ TopProc
+     [ "-t", concatMap nameAndCpu [1..10]
+     ] 30
+   ]
+  , sepChar = "%"
+  , alignSep = "}{"
+  , template = "%topmem% }{ %top%"
+  }
+ where
+  backgroundColor = blackColor
+  foregroundColor = blueColor
+  (nameAndMem ∷ Int → String) = \n → concat
+    [ withColor blueColor "("
+    , withColor blueColorKeyboardLayout $ printf "<name%d> " n
+    , withColor yellowColor $ printf "<mem%d>" n
+    , withColor blueColor ") "
+    ]
+  (nameAndCpu ∷ Int → String) = \n → concat
+    [ withColor blueColor "("
+    , withColor blueColorKeyboardLayout $ printf "<name%d> " n
+    , withColor yellowColor $ printf "<cpu%d>" n
+    , withColor blueColor ") "
+    ]
+
+
+withColor ∷ Color → String → String
+withColor = printf "<fc=%s>%s</fc>"
+
+blackColor = "#080808"
+blueColor = "#2c3c3c"
+blueColorKeyboardLayout = "#11eebb"
+yellowColor = "#ffd317"

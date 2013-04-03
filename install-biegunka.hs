bc3f4d22b36b93b68e163a0a14913e558275fd1b
Dmitry Malikov <malikov.d.y@gmail.com>
2013-04-03 21:50:33 +0400

    Split profile into a separate profiles

--- install-biegunka.hs
+++ install-biegunka.hs
@@ -9,95 +9,199 @@ import           Biegunka.Source.Git
 
 main ∷ IO ()
 main =
-  biegunka (set root "~") p $ execute def
+  biegunka (set root "~") profiles $ execute def
  where
-  p = profile "mine" $ do
-    dotfiles
-    gitflow
-    hpasteit
-    urxvt_tabbedex
-    vim_pathogen
-    vim_pathogen_modules
-    xmonad
+  profiles = sequence_
+    [ vim
+    , xmonad
+    , git'
+    , ruby
+    , x
+    , ghc
+    , irssi
+    , mpd
+    , mplayer
+    , pentadactyl
+    , screen
+    , ackrc
+    , apvlv
+    , shell'
+    , java
+    , conky
+    , gtk
+    , mocp
+    , tmux
+    , uzbl
+    , vifm
+    , misc
+    ]
 
+git_dotfiles = git "git@github.com:dmalikov/dotfiles" "projects/dmalikov/dotfiles"
 
-dotfiles = git "git@github.com:dmalikov/dotfiles" "projects/dmalikov/dotfiles" $ links
-  [ ( "ackrc", ".ackrc" )
-  , ( "XCompose", ".XCompose" )
-  , ( "Xdefaults", ".Xdefaults" )
-  , ( "Xresources.large", ".Xresources.large" )
-  , ( "Xresources.scratchpad", ".Xresources.scratchpad" )
-  , ( "Xresources.shiva", ".Xresources.shiva" )
-  , ( "apvlvrc", ".apvlvrc" )
-  , ( "bashrc", ".bashrc" )
-  , ( "bleedie.xml", ".IdeaIC12/config/colors/bleedie.xml" )
-  , ( "buftabs.js", ".pentadactyl/plugins/buftabs.js" )
-  , ( "conkyrc", ".conkyrc" )
-  , ( "ghci", ".ghci" )
-  , ( "gitconfig", ".gitconfig" )
-  , ( "gitignore", ".gitignore" )
-  , ( "gtkrc-2.0.mine", ".gtkrc-2.0.mine" )
-  , ( "haskell.vim", ".vim/after/syntax/haskell.vim" )
-  , ( "inputrc", ".inputrc" )
-  , ( "irbrc", ".irbrc" )
-  , ( "irssi.bleeding.theme", ".irssi/bleeding.theme" )
-  , ( "irssi.config", ".irssi/config" )
-  , ( "mocpconfig", ".moc/config" )
-  , ( "mpdconf", ".mpdconf" )
-  , ( "mplayer.config", ".mplayer/config" )
-  , ( "mplayer.input.conf", ".mplayer/input.conf" )
-  , ( "ncmpcppconf", ".ncmpcpp/config" )
-  , ( "neverland-darker.vim", ".vim/colors/neverland-darker.vim" )
-  , ( "pemees.penta", ".pentadactyl/colors/pemees.penta" )
-  , ( "pentadactylrc", ".pentadactylrc" )
-  , ( "prexinit", "prexinit" )
-  , ( "rvmrc", ".rvmrc" )
-  , ( "screenrc", ".screenrc" )
-  , ( "tigrc", ".tigrc" )
-  , ( "tmux.conf", ".tmux.conf" )
-  , ( "uzbl-config", ".config/uzbl/config" )
-  , ( "vifm-neverland.colorscheme", ".vifm/colors/neverland" )
-  , ( "vifmrc", ".vifm/vifmrc" )
-  , ( "vimrc", ".vimrc" )
-  , ( "xinitrc", ".xinitrc" )
-  , ( "xmobar-top.hs", ".xmonad/xmobar-top.hs" )
-  , ( "xmobar.hs", ".xmonad/xmobar.hs" )
-  , ( "xmobarrc", ".xmobarrc" )
-  , ( "xmonad.hs", ".xmonad/xmonad.hs" )
-  , ( "zshrc", ".zshrc" )
-  ]
+vim :: Script Profiles
+vim = do
+  profile "vim_pathogen" $ do
+    git "git@github.com:tpope/vim-pathogen.git" "projects/misc/vim-pathogen" $
+      link "autoload/pathogen.vim" ".vim/autoload/pathogen.vim"
+  profile "vim_pathogen_modules" $ do
+    git "git@github.com:Shougo/vimproc.git" ".vim/bundle/vimproc" $
+      shell "make -f make_unix.mak"
+    git_ "git@github.com:Shougo/neocomplcache.git" ".vim/bundle/neocomplcache"
+    git_ "git@github.com:Shougo/unite.vim.git" ".vim/bundle/unite"
+    git_ "git@github.com:airblade/vim-gitgutter.git" ".vim/bundle/gitgutter"
+    git_ "git@github.com:dahu/Insertlessly.git" ".vim/bundle/Insertlessly"
+    git_ "git@github.com:godlygeek/tabular.git" ".vim/bundle/tabular"
+    git_ "git@github.com:scrooloose/syntastic.git" ".vim/bundle/syntastic"
+    git_ "git@github.com:spolu/dwm.vim.git" ".vim/bundle/dwm"
+    git_ "git@github.com:supki/vim-perds.git" ".vim/bundle/perds"
+    git_ "git@github.com:tpope/vim-commentary.git" ".vim/bundle/commentary"
+    git_ "git@github.com:tpope/vim-markdown.git" ".vim/bundle/markdown"
+    git_ "git@github.com:tpope/vim-surround.git" ".vim/bundle/surround"
+    git_ "git@github.com:ujihisa/neco-ghc.git" ".vim/bundle/neco-ghc"
+  profile "vim_rc" $
+    git_dotfiles $ link "vimrc" ".vimrc"
+  profile "vim_syntax" $
+    git_dotfiles $ link "haskell.vim" ".vim/after/syntax/haskell.vim"
+  profile "vim_colorschemes" $
+    git_dotfiles $ link "neverland-darker.vim" ".vim/colors/neverland-darker.vim"
 
-vim_pathogen = git "git@github.com:tpope/vim-pathogen.git" "projects/misc/vim-pathogen" $
-  link "autoload/pathogen.vim" ".vim/autoload/pathogen.vim"
-
-vim_pathogen_modules = do
-  git "git@github.com:Shougo/vimproc.git" ".vim/bundle/vimproc" $ shell "make -f make_unix.mak"
-  git_ "git@github.com:Shougo/neocomplcache.git" ".vim/bundle/neocomplcache"
-  git_ "git@github.com:Shougo/unite.vim.git" ".vim/bundle/unite"
-  git_ "git@github.com:airblade/vim-gitgutter.git" ".vim/bundle/gitgutter"
-  git_ "git@github.com:bitc/vim-hdevtools.git" ".vim/bundle/hdevtools"
-  git_ "git@github.com:dahu/Insertlessly.git" ".vim/bundle/Insertlessly"
-  git_ "git@github.com:eagletmt/ghcmod-vim.git" ".vim/bundle/ghcmod-vim"
-  git_ "git@github.com:godlygeek/tabular.git" ".vim/bundle/tabular"
-  git_ "git@github.com:rosstimson/scala-vim-support" ".vim/bundle/scala-support"
-  git_ "git@github.com:scrooloose/syntastic.git" ".vim/bundle/syntastic"
-  git_ "git@github.com:spolu/dwm.vim.git" ".vim/bundle/dwm"
-  git_ "git@github.com:supki/vim-perds.git" ".vim/bundle/perds"
-  git_ "git@github.com:tpope/vim-commentary.git" ".vim/bundle/commentary"
-  git_ "git@github.com:tpope/vim-markdown.git" ".vim/bundle/markdown"
-  git_ "git@github.com:tpope/vim-surround.git" ".vim/bundle/surround"
-  git_ "git@github.com:ujihisa/neco-ghc.git" ".vim/bundle/neco-ghc"
-
-
-urxvt_tabbedex = git "git@github.com:stepb/urxvt-tabbedex.git" "projects/misc/urxvt-tabbedex" $
-  link "tabbedex" ".urxvt/perl/tabbedex"
-
-gitflow = git_ "git@github.com:nvie/gitflow.git" "projects/misc/gitflow"
-hpasteit = git_ "git@github.com:parcs/hpasteit.git" "projects/misc/hpasteit"
-
+xmonad :: Script Profiles
 xmonad = do
-  darcs_ "http://code.haskell.org/xmonad" "projects/misc/xmonad"
-  darcs_ "http://code.haskell.org/XMonadContrib" "projects/misc/XMonadContrib"
+  profile "xmonad_darcs_repos" $ do
+    darcs_ "http://code.haskell.org/xmonad" "projects/misc/xmonad"
+    darcs_ "http://code.haskell.org/XMonadContrib" "projects/misc/xmonad-contrib"
+    darcs_ "http://code.haskell.org/xmonad-extras/" "projects/misc/xmonad-extras"
+  profile "xmonad_xmonad.hs" $
+    git_dotfiles $ link "xmonad" ".xmonad/xmonad.hs"
+  profile "xmonad_xmobar" $ do
+    git_dotfiles $ do
+      link "xmobar-top.hs" ".xmonad/xmobar-top.hs"
+      link "xmobar.hs" ".xmonad/xmobar.hs"
+      link "xmobarrc" ".xmobarrc"
 
+git' :: Script Profiles
+git' = profile "git" $ do
+  git_dotfiles $ do
+    link "gitconfig" ".gitconfig"
+    link "gitignore" ".gitignore"
+    link "tigrc" ".tigrc"
+  git_ "git@github.com:nvie/gitflow.git" "projects/misc/gitflow"
 
-links = mapM_ $ uncurry link
+ruby :: Script Profiles
+ruby = profile "ruby" $ do
+  git_dotfiles $ do
+    link "irbrc" ".irbrc"
+    link "rvmrc" ".rvmrc"
+
+x :: Script Profiles
+x = profile "X" $ do
+  git_dotfiles $ do
+    link "XCompose" ".XCompose"
+    link "Xdefaults" ".Xdefaults"
+    link "Xresources.large" ".Xresources.large"
+    link "Xresources.scratchpad" ".Xresources.scratchpad"
+    link "Xresources.shiva" ".Xresources.shiva"
+    link "inputrc" ".inputrc"
+    link "prexinit" "prexinit"
+    link "xinitrc" ".xinitrc"
+
+ghc :: Script Profiles
+ghc = profile "ghc" $ do
+  git_dotfiles $
+    link "ghci" ".ghci"
+  git_ "git@github.com:eagletmt/ghcmod-vim.git" ".vim/bundle/ghcmod-vim"
+  git_ "git@github.com:bitc/vim-hdevtools.git" ".vim/bundle/hdevtools"
+
+irssi :: Script Profiles
+irssi = profile "irssi" $
+  git_dotfiles $ do
+    link "irssi.bleeding.theme" ".irssi/bleeding.theme"
+    link "irssi.config" ".irssi/config"
+
+mpd :: Script Profiles
+mpd = profile "mpd" $
+  git_dotfiles $ do
+    link "mpdconf" ".mpdconf"
+    link "ncmpcppconf" ".ncmpcpp/config"
+
+mplayer :: Script Profiles
+mplayer = profile "mplayer" $
+  git_dotfiles $ do
+    link "mplayer.config" ".mplayer/config"
+    link "mplayer.input.conf" ".mplayer/input.conf"
+
+pentadactyl :: Script Profiles
+pentadactyl = profile "pentadactyl" $
+  git_dotfiles $ do
+    link "pemees.penta" ".pentadactyl/colors/pemees.penta"
+    link "pentadactylrc" ".pentadactylrc"
+    link "buftabs.js" ".pentadactyl/plugins/buftabs.js"
+
+screen :: Script Profiles
+screen = profile "screen" $
+  git_dotfiles $
+    link "screenrc" ".screenrc"
+
+ackrc :: Script Profiles
+ackrc = profile "ack" $
+  git_dotfiles $
+    link "ackrc" ".ackrc"
+
+apvlv :: Script Profiles
+apvlv = profile "apvlv" $
+  git_dotfiles $
+    link "apvlvrc" ".apvlvrc"
+
+shell' :: Script Profiles
+shell' = do
+  profile "shell_bash" $
+    git_dotfiles $
+      link "bashrc" ".bashrc"
+  profile "shell_zsh" $
+    git_dotfiles $
+      link "zshrc" ".zshrc"
+
+java :: Script Profiles
+java = do
+  profile "idea" $
+    git_dotfiles $
+      link "bleedie.xml" ".IdeaIC12/config/colors/bleedie.xml"
+
+conky :: Script Profiles
+conky = profile "conky" $
+  git_dotfiles $
+    link "conkyrc" ".conkyrc"
+
+gtk :: Script Profiles
+gtk = profile "gtk" $
+  git_dotfiles $
+    link "gtkrc-2.0.mine" ".gtkrc-2.0.mine"
+
+mocp :: Script Profiles
+mocp = profile "mocp" $
+  git_dotfiles $
+    link "mocpconfig" ".moc/config"
+
+tmux :: Script Profiles
+tmux = profile "tmux" $
+  git_dotfiles $
+    link "tmux.conf" ".tmux.conf"
+
+uzbl :: Script Profiles
+uzbl = profile "uzbl" $
+  git_dotfiles $
+    link "uzbl-config" ".config/uzbl/config"
+
+vifm :: Script Profiles
+vifm = profile "vifm" $
+  git_dotfiles $ do
+    link "vifm-neverland.colorscheme" ".vifm/colors/neverland"
+    link "vifmrc" ".vifm/vifmrc"
+
+misc :: Script Profiles
+misc = do
+  profile "misc_urxvt_tabbedex" $
+    git "git@github.com:stepb/urxvt-tabbedex.git" "projects/misc/urxvt-tabbedex" $
+      link "tabbedex" ".urxvt/perl/tabbedex"
+  profile "misc_hpasteit" $
+    git_ "git@github.com:parcs/hpasteit.git" "projects/misc/hpasteit"

22992fefbd7111cef16b43fcc7b7e50e11c042db
Dmitry Malikov <malikov.d.y@gmail.com>
2013-07-19 00:59:34 +0400

    bye-bye T510 :[

--- biegunka/Main.hs
+++ biegunka/Main.hs
@@ -1,14 +1,13 @@
 {-# LANGUAGE DataKinds, TemplateHaskell #-}
 module Main where
 
-import qualified Environment.T510 as T510
 import qualified Environment.X220 as X220
 
 import Control.Lens (set)
 
 import Control.Biegunka
 
-data Environment = X220 | T510
+data Environment = X220 | MacBookPro
 
 makeOptionsParser ''Environment
 
@@ -17,4 +16,4 @@ main = do
   (env, r) <- optionsParser
   case env of
     X220 -> r (set root "~") (set templates $ Templates X220.settings) X220.profiles
-    T510 -> r (set root "~") (set templates $ Templates T510.settings) T510.profiles
+    MacBookPro -> error "not ready yet"

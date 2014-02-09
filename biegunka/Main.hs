eff2f3461862d02696e49e4050b9481730795e11
Dmitry Malikov <malikov.d.y@gmail.com>
2014-02-10 00:04:41 +0400

    Latest bee-er-goon-k-ah

--- biegunka/Main.hs
+++ biegunka/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DataKinds, TemplateHaskell #-}
+{-# LANGUAGE DataKinds, DeriveDataTypeable #-}
 module Main where
 
 import qualified Environment.X220 as X220
@@ -9,12 +9,11 @@ import Control.Biegunka
 import Control.Biegunka.Templates.HStringTemplate
 
 data Environment = X220 | MacBookPro
-
-biegunkaOptions ''Environment
+  deriving (Data, Typeable)
 
 main :: IO ()
 main = do
-  (env, r) <- options
+  (env, r) <- options [X220, MacBookPro]
   case env of
     X220 -> r (set root "~" . set templates (hStringTemplate X220.settings)) X220.profiles
     MacBookPro -> error "not ready yet"

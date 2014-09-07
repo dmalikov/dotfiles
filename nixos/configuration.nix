509fde4fb2c6e73945a63e9fcfd2333810e66615
Dmitry Malikov <malikov.d.y@gmail.com>
2014-09-07 19:39:04 +0200

    Add nixos/configuration.nix

---
+++ nixos/configuration.nix
@@ -0,0 +1,75 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  # Define on which hard drive you want to install Grub.
+  boot.loader.grub.device = "/dev/sda";
+
+  nixpkgs.config.allowUnfree = true;
+
+  networking.firewall.enable = false;
+  networking.hostName = "nixos"; # Define your hostname.
+  networking.wireless.enable = true;  # Enables wireless.
+
+  # Select internationalisation properties.
+  i18n = {
+    consoleFont = "ter-116n";
+    consoleKeyMap = "us";
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  fonts = {
+    enableFontDir = true;
+    enableGhostscriptFonts = true;
+    fonts = [
+      pkgs.terminus_font
+    ];
+  };
+
+  environment = with pkgs; { 
+    systemPackages = [
+      curl
+      dhcp
+      git
+      htop
+      iotop
+      nix-repl
+      nmap
+      tig
+      tmux
+      tree
+      vim
+      wget
+      wgetpaste
+    ];
+  };
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  services.transmission = {
+    enable = true;
+  };
+
+  users.extraUsers.yep = {
+    name = "yep";
+    group = "users";
+    uid = 1000;
+    createHome = true;
+    home = "/home/yep";
+    extraGroups = [ "wheel" ];
+    useDefaultShell = true;
+  };
+ 
+}

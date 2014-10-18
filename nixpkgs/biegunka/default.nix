fa0730eb226bf1dd3425a9dd198d92acad2064b5
Dmitry Malikov <malikov.d.y@gmail.com>
2014-10-18 20:41:07 +0400

    [nixpkgs] make biegunka avoid optparse-applicative-0.11

--- nixpkgs/biegunka/default.nix
+++ nixpkgs/biegunka/default.nix
@@ -3,7 +3,7 @@
 { cabal, acidState, aeson, ansiWlPprint, async, commandQq
 , dataDefaultClass, directoryLayout, exceptions, filepath, free
 , hspec, hspecExpectationsLens, HStringTemplate, lens, meep, mtl
-, optparseApplicative, pointed, reflection, safecopy, semigroups
+, optparseApplicative_0_10_0, pointed, reflection, safecopy, semigroups
 , stm, tagged, taggedTransformer, temporary, terminalSize, text
 , transformers, void
 }:
@@ -17,12 +17,12 @@ cabal.mkDerivation (self: {
   buildDepends = [
     acidState aeson ansiWlPprint async commandQq dataDefaultClass
     directoryLayout exceptions filepath free hspec HStringTemplate lens
-    meep mtl optparseApplicative pointed reflection safecopy semigroups
+    meep mtl optparseApplicative_0_10_0 pointed reflection safecopy semigroups
     stm tagged taggedTransformer terminalSize text transformers void
   ];
   testDepends = [
     dataDefaultClass directoryLayout filepath free hspec
-    hspecExpectationsLens lens optparseApplicative semigroups temporary
+    hspecExpectationsLens lens optparseApplicative_0_10_0 semigroups temporary
     text transformers
   ];
   meta = {

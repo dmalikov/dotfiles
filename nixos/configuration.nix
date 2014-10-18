524b312882f0706e09cbb8f5177310834ba820bc
Dmitry Malikov <malikov.d.y@gmail.com>
2014-10-19 01:05:10 +0400

    [nixos/ssh] disable password authentication

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -81,7 +81,10 @@
     '';
   };
 
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    passwordAuthentication = false;
+  };
 
   services.transmission = {
     enable = true;
@@ -101,6 +104,7 @@
       home = "/home/yep";
       extraGroups = [ "wheel" "audio" ];
       useDefaultShell = true;
+      openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCjDXb8JmdWxW40RS7TMm8xz9kadlo+CAdYrgGps5cIAXBD6+fdLbEjB66vu9YxG/WUUN7lOdRvh/aYJqzh+yLGD06/JSTZXdn8NnCkYBV70bUNzp/GqYaX+t7UdL8eF60p8HOQQVmOYUjbPX0eBEi4b6iDoT+3mO2m5CbLl19iJadTjB0lbJyRNC6BECHhUC+bCXaCLRLTr66f8qShAXup7UAi9v92UXPfm0c+ZaVWoIEjQnRH15VJoFo6geHhmUTSK8pJq+pUCpEiP9ryV/7fBuw2o9OVrP95dxt48cvjOnXq4wGj+DKvG7eCXKM8jC3tJOaUAktgk4Ky/x/fXv+v" ];
     };
   };
 }

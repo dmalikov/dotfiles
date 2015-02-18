369f9cfcde8bc55ff6541b379ee310b618eff83f
Dmitry Malikov <malikov.d.y@gmail.com>
2015-02-19 00:18:04 +0100

    Prune w530 environment

--- biegunka/Main.hs
+++ biegunka/Main.hs
@@ -5,14 +5,11 @@ import           Control.Biegunka
 
 import           Environment.Defaults
 import qualified Environment.S10      as S10
-import qualified Environment.W530     as W530
 
 instance Environmentable Environment where
   configs S10  = S10.configs
-  configs W530 = W530.configs
 
   profiles S10  = S10.profiles
-  profiles W530 = W530.profiles
 
 main :: IO ()
-main = uncurry rollout =<< options [S10, W530]
+main = uncurry rollout =<< options [S10]
